04b90d761d1bcd11cf80913bc4043c2b3d088046
megatzafran <2021854946@student.uitm.edu.my>
2022-09-14 04:25:31 +0800

    promisifying

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -167,7 +167,6 @@ btn.addEventListener('click', function () {
   getCountryData('Portugal');
 });
 
-  */
 
 console.log(`Test Start`);
 setTimeout(() => console.log(`0 sec timer`), 0);
@@ -178,3 +177,36 @@ Promise.resolve(`Resolved promise 2`).then(res => {
 });
 
 console.log('Test end');
+
+*/
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log(`Lottery draw is happening`);
+
+  setTimeout(function () {
+    if (Math.random() >= 0.5) {
+      resolve(`You WIN`);
+    } else {
+      reject(new Error(`You LOST`));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(err => console.error(err));
+
+//Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, seconds * 1000);
+  });
+};
+
+wait(2)
+  .then(() => {
+    console.log('I waited for 2 seconds');
+    return wait(1);
+  })
+  .then(() => console.log(`I waited for 1 second`));
+
+Promise.resolve('abc').then(x => console.log(x));
+Promise.reject('abc').catch(err => console.error(err));
